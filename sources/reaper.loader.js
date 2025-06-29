a3cf9499e9229999ce3b58729883ac23ee6d41c6
Alexandre Blondeau <deformhead@gmail.com>
2025-06-29 17:07:25 +0200

    change error behavior

--- sources/reaper.loader.js
+++ sources/reaper.loader.js
@@ -21,24 +21,24 @@ module.exports = function loader() {
     const options = context.getOptions();
     const reaper = options.reaper;
 
-    if (typeof reaper === 'undefined') {
-
-        throw new Error('Reaper executable path is missing in the loader options.');
-    }
-
-    if (fs.existsSync(reaper) === false) {
-
-        throw new Error('Reaper executable not found reading path: ' + (typeof reaper !== 'undefined' || reaper !== '' ? '\'' + reaper + '\'' : '<empty>') + '.');
-    }
-
-    const location = path.dirname(file);
-    const filename = path.basename(file, '.rpp');
-    const source = filename + '.wav';
-
     try {
+
+        const location = path.dirname(file);
+        const filename = path.basename(file, '.rpp');
+        const source = filename + '.wav';
 
         if (fs.existsSync(path.resolve(location, source)) === false
         || fs.statSync(path.resolve(location, source)).mtime < fs.statSync(file).mtime) {
+
+            if (typeof reaper === 'undefined') {
+
+                throw new Error('Reaper executable path is missing in the loader options.');
+            }
+
+            if (fs.existsSync(reaper) === false) {
+
+                throw new Error('Reaper executable not found reading path: ' + (typeof reaper !== 'undefined' || reaper !== '' ? '\'' + reaper + '\'' : '<empty>') + '.');
+            }
 
             subprocess.execSync(
 
